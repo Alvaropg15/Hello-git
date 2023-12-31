dc292d29350b15f76d89ef63ebb5d0eb1f001cf6
Alvaro <alv.parrilla.dev@gmail.com>
2023-12-31 08:09:25 +0100

    Ultimos cambios nates del descanso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("New Hola, Git!");
+        System.out.println("New Hola, Git! con cambios");
     }
 }
