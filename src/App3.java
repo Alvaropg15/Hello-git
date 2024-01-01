43fc3c9be253c944703ffe61aa00285fae451802
Alvaro <alv.parrilla.dev@gmail.com>
2024-01-01 09:36:24 +0100

    App 3 v2

--- src/App3.java
+++ src/App3.java
@@ -1,6 +1,6 @@
 public class App3 {
     public static void main(String[] args) {
-        System.out.println("Git 3 compadreeeee");
+        System.out.println("Git 3 compadreeeee v2");
     }
     
 }
