c381b1ad6db00cd2690802e9950790c0d3760cc6
Alvaro <alv.parrilla.dev@gmail.com>
2024-01-10 20:37:26 +0100

    Hola GitHub

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("New Hola, Git! con cambios");
+        System.out.println("Hola, GitHub!");
     }
 }
